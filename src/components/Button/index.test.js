7696f0db8ccbc1b79f1f7529210f53b45462ba14
baimbrams <baimz.bramz@gmail.com>
2024-05-13 22:10:04 +0700

    "app Travel"

---
+++ src/components/Button/index.test.js
@@ -0,0 +1,36 @@
+/* eslint-disable testing-library/prefer-screen-queries */
+/* eslint-disable testing-library/no-node-access */
+/* eslint-disable testing-library/no-container */
+import React from "react";
+import { render } from "@testing-library/react";
+import Button from "./index";
+import { BrowserRouter as Router } from "react-router-dom";
+
+test("should not allowed click on disabled button", () => {
+  const { container } = render(<Button isDisabled></Button>);
+
+  expect(container.querySelector("span.disabled")).toBeInTheDocument();
+});
+
+test("should render loading/spinner button", () => {
+  const { container, getByText } = render(<Button isLoading></Button>);
+
+  expect(getByText(/loading/i)).toBeInTheDocument();
+  expect(container.querySelector("span.spinner-border")).toBeInTheDocument();
+});
+
+test("should render <a> tag", () => {
+  const { container } = render(<Button type="link" isExternal></Button>);
+
+  expect(container.querySelector("a")).toBeInTheDocument();
+});
+
+test("should render <Link> components", () => {
+  const { container } = render(
+    <Router>
+      <Button href="" type="link"></Button>
+    </Router>
+  );
+
+  expect(container.querySelector("a")).toBeInTheDocument();
+});
