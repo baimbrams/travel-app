2f85322f5e626e880527dcac3ce4b19081a6f751
baimbrams <baimz.bramz@gmail.com>
2024-05-13 23:39:09 +0700

    "landing page"

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from "react";
 import "assets/scss/style.scss";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import LandingPage from "pages/LandingPage";
 
 function App() {
-  return <div className="App">Halo dunia</div>;
+  return (
+    <div className="App">
+      <Router>
+        <Route path="/">
+          <LandingPage />
+        </Route>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/LandingPage.js
@@ -0,0 +1,10 @@
+import Header from "layout/Header";
+import React from "react";
+
+export default function LandingPage(props) {
+  return (
+    <>
+      <Header {...props}></Header>;
+    </>
+  );
+}
