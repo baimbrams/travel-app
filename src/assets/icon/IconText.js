3bb4223a1d2ab1f42290d62db44022230b0f23d7
baimbrams <baimz.bramz@gmail.com>
2024-05-13 22:49:19 +0700

    "variable colors"

---
+++ src/assets/icon/IconText.js
@@ -0,0 +1,10 @@
+import Button from "components/Button";
+import React from "react";
+
+export default function IconText() {
+  return (
+    <Button type="link" href="" className="brand-text-icon">
+      Travel <span className="text-gray-900">OnSPot</span>
+    </Button>
+  );
+}
